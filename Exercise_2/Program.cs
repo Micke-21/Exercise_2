9a8a7fa50faf95910247296cde95537efe82d959
Mikael Larsson <mikael.larsson@wsp.com>
2021-12-01 16:30:28 +0100

    Add free for children and 100 year, and removed blanks in choice 3

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise_2
 {
@@ -37,10 +38,14 @@ namespace Exercise_2
 
                         if (int.TryParse(Console.ReadLine(), out int age))
                         {
-                            if (age < 20)
+                            if (age < 5)
+                                Console.WriteLine("Ungdomspris: Gratis");
+                            else if (age < 20)
                                 Console.WriteLine("Ungdomspris: 80 kr");
-                            else if (age > 64)
+                            else if (age > 64 && age < 101)
                                 Console.WriteLine("Pensionärspris: 90 kr");
+                            else if (age > 100)
+                                Console.WriteLine("Pensionärspris: Gratis");
                             else
                                 Console.WriteLine("Standardpris 120 kr");
                         }
@@ -48,6 +53,7 @@ namespace Exercise_2
                         {
                             Console.WriteLine("Du angav inte en ålder.");
                         }
+                        Console.WriteLine();
                         break;
 
                     case "4": //  Pris för helt sällskap
@@ -58,13 +64,13 @@ namespace Exercise_2
                             for (int i = 0; i < antBes; i++)
                             {
                                 Console.Write("Ange ålder för besökare {0} ", i + 1);
-                                if (int.TryParse(Console.ReadLine(), out int ageN))
+                                if (int.TryParse(Console.ReadLine(), out int ageN)) // under 5 och över 100 gratis
                                 {
-                                    if (ageN < 20)
+                                    if (ageN >= 5 && ageN < 20)             // 5 to 19 Ungdom
                                         totalCost += 80;
-                                    else if (ageN > 64)
+                                    else if (ageN > 64 && ageN < 101)       // 65 to 100 Pensionär
                                         totalCost += 90;
-                                    else
+                                    else if (ageN <= 64 && ageN >= 20)      // 20 to 64 Stantadpris
                                         totalCost += 120;
                                 }
                                 else
@@ -82,16 +88,17 @@ namespace Exercise_2
                         string myText = Console.ReadLine();
                         for (int i = 0; i < 10; i++)
                         {
-                            Console.Write($"{i+1} {myText}");
+                            Console.Write($"{i + 1} {myText}");
                         }
                         Console.WriteLine();
                         break;
                     case "3": // Det treje ordet
                         Console.WriteLine("Ange en mening på minst tre ord");
                         string mySentence = Console.ReadLine();
-
-                        string[] myWords = mySentence.Split(" ");
-                        if ( myWords.Length < 3)
+                        
+                        string[] myWords = mySentence.Trim().Split(" ");
+                        myWords = myWords.Where(x => !string.IsNullOrEmpty(x)).ToArray(); // remove the blanks in the array
+                        if (myWords.Length < 3)
                         {
                             Console.WriteLine("Det måste var minst tre ord i meningen!");
                         }
